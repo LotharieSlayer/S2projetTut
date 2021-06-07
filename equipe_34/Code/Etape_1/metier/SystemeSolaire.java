629a5d1187513398686ee36f30f45ba1530fb4e0
Tom-Glamour <83942459+Tom-Glamour@users.noreply.github.com>
2021-06-07 14:30:17 +0200

    Add files via upload

--- equipe_34/Code/Etape_1/metier/SystemeSolaire.java
+++ equipe_34/Code/Etape_1/metier/SystemeSolaire.java
@@ -2,13 +2,21 @@ import java.util.ArrayList;
 
 public class SystemeSolaire {
 
-    private String nom;
-    private int position;
+    private String nom     ;
+    private int    position;
+    private int    nbPlanet;
 
-	public SystemeSolaire (String nom)
+	public SystemeSolaire (String nom, int position, int nbPlanet)
 	{
-        this.nom = nom;
+        this.nom      = nom     ;
+        this.position = position;
+        this.nbPlanet = nbPlanet;
     }
+    
+    public SystemeSolaire (String nom, int position, int nbPlanet)
+    {
+    		if (nom != "Triälum" || nom != "Uninium" || nom != "Mervelléum" || nom != "Quintum") return null;
+    		else return new SystemeSolaire
     
 	/*public String toString ()
 	{
