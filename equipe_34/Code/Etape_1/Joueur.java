04edafcf0d5c69c0da843447f4eece8317659f83
Etienne Chevrollier <etienne.chevrollier@live.fr>
2021-06-07 15:28:09 +0200

    Améliorations Classe Métier --> Galaxie.java

---
+++ equipe_34/Code/Etape_1/Joueur.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Joueur {
+
+    private String nom;
+    private String couleurSeigneur;
+
+    private ArrayList<Planete> planetesControlees;
+
+	public Joueur ( String nom, String couleurSeigneur )
+	{
+        this.nom = nom;
+        this.couleurSeigneur = couleurSeigneur;
+    }
+    
+    public static Joueur creerJoueur ( String nom, String couleurSeigneur )
+	{
+		if ( nom == null || couleurSeigneur != "Noir" && couleurSeigneur != "Blanc")  return null;
+
+		return new Joueur (nom, couleurSeigneur);
+	}
+
+    public String getNom() { return this.nom; }
+
+    public void ajouterPlanete(Planete planete)
+    {
+        planetesControlees.add(planete);
+    };
+
+	public String toString()
+	{
+		return String.format ( "%-10s", this.nom, " : le Seigneur des Anneaux " + this.couleurSeigneur );
+	}
+    
+}
---
+++ equipe_34/Code/Etape_1/SystemeSolaire.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class SystemeSolaire {
+
+    private String nom     ;
+    private int    nbPlanete;
+
+	public SystemeSolaire (String nom, int nbPlanete)
+	{
+        this.nom      = nom     ;
+        this.nbPlanete = nbPlanete;
+    }
+    
+    public SystemeSolaire creerSystemeSolaire (String nom, int nbPlanete)
+    {
+    		if (nom != "Triälum" || nom != "Uninium" || nom != "Mervelléum" || nom != "Quintum") return null;
+            else return new SystemeSolaire(nom, nbPlanete);
+    }
+
+    public String getNom(){ return this.nom; }
+
+	/*public String toString ()
+	{
+    }*/
+}
