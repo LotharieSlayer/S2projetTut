dd55ecd756ceb6ca48a86f8dbe208d5726c27174
Etienne Chevrollier <etienne.chevrollier@live.fr>
2021-06-07 11:12:29 +0200

    Commentaire Action

--- equipe_34/Code/Etape_1/Controleur.java
+++ equipe_34/Code/Etape_1/Controleur.java
@@ -1,10 +1,17 @@
+package lesseigneursdutemps;
+
 import java.util.Scanner;
+import java.io.*;
+import java.awt.Point;
 
 public class Controleur
 {
-    public Controleur
+    public  static final String LANGUE = "FR";  // FR/ENG
+
+    public Controleur()
     {
-        private String action ()
+
+        /*public String action ()
         {
             String action;
             Scanner clavier = new Scanner(System.in);
@@ -19,19 +26,38 @@ public class Controleur
             }
             else
             {
-                System.out.println("Action saisie est indisponible")
+                System.out.println("Action saisie est indisponible");
             }
     
             // fermer les ressources
             clavier.close();
-        }
+        }*/
 
-        private String toString()
+        String action;
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("Saisir votre action : ");
+        action = clavier.nextLine();
+        
+        if (action == "n" || action == "b")
+        {
+                String planete;
+                System.out.print("Saisir la planete de votre choix : ");
+                planete = clavier.nextLine();
+        }
+        else
+        {
+            System.out.println("Action saisie est indisponible");
+        }
+    
+        // fermer les ressources
+        clavier.close();
+
+        /*private String toString()
         {
             while (numSystem <= 4)
             {
-                Systeme.out.println("Systeme" + numSystem + planete dispo:" + nbDispo + "planete Noir"+ nbNoir + "planete Blanche" + nbBlanche);
+                Systeme.out.println("Systeme" + numSystem + "planete dispo:" + nbDispo + "planete Noir" + nbNoir + "planete Blanche" + nbBlanche);
             }
-        }
+        }*/
     }
 }
