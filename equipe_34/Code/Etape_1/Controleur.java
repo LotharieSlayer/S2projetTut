171bb4e1f13e858ad9f3cdb0087d427edebeefde
Danylapoule <70890762+Danylapoule@users.noreply.github.com>
2021-06-07 10:36:09 +0200

    Update Controleur.java

---
+++ equipe_34/Code/Etape_1/Controleur.java
@@ -0,0 +1,13 @@
+public class Controleur
+{
+    public Controleur
+    {
+        private String toString()
+        {
+            while (numSystem <= 4)
+            {
+                Systeme.out.println("Systeme" + numSystem + planete dispo:" + nbDispo + "planete Noir"+ nbNoir + "planete Blanche" + nbBlanche);
+            }
+        }
+    }
+}
