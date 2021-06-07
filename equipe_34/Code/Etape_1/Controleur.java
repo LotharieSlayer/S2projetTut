c500fb102b7788ceb6fcad7c05846838dffafffb
Danylapoule <70890762+Danylapoule@users.noreply.github.com>
2021-06-07 10:44:56 +0200

    Update Controleur.java

--- equipe_34/Code/Etape_1/Controleur.java
+++ equipe_34/Code/Etape_1/Controleur.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Controleur
 {
     public Controleur
     {
+        private String action ()
+        {
+            String action;
+            Scanner clavier = new Scanner(System.in);
+            System.out.print("Saisir votre action : ");
+            action = clavier.nextLine();
+
+            if (action == "n" || action == "b")
+            {
+                String planete;
+                System.out.print("Saisir la planete de votre choix : ");
+                planete = clavier.nextLine();
+            }
+            else
+            {
+                System.out.println("Action saisie est indisponible")
+            }
+    
+            // fermer les ressources
+            clavier.close();
+        }
+
         private String toString()
         {
             while (numSystem <= 4)
