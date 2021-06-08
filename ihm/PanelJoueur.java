0d817d693d31e61cc77e894a65a6989697c69645
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2021-06-08 14:36:35 +0200

    IHM : Affichage des planètes

--- ihm/PanelJoueur.java
+++ ihm/PanelJoueur.java
@@ -9,22 +9,35 @@ public class PanelJoueur extends JPanel
 {
 	private Controleur  ctrl;
 	private JPanel		pnlMain;
-	private JLabel		test = new JLabel();
 
 	private PanelSelectSysteme     panelSelectTrialum;
 	private PanelSelectSysteme     panelSelectUninium;
 	private PanelSelectSysteme     panelSelectMervelleum;
 	private PanelSelectSysteme     panelSelectQuintum;
+	private PanelCentre            pnlCentre;
 
 
 	public PanelJoueur(Controleur ctrl)
 	{
 		this.ctrl = ctrl;
+		this.setBackground(Color.WHITE);
+
 		pnlMain = new JPanel();
 		this.pnlMain.setLayout( new GridLayout(0,5) );
-		this.setBackground(Color.WHITE);
+
+		this.pnlCentre = new PanelCentre();
+		this.pnlCentre.setLayout( new GridLayout(2,0));
 		this.panelSelectTrialum = new PanelSelectSysteme();//, this.ctrl);
+		this.panelSelectUninium = new PanelSelectSysteme();//, this.ctrl);
+		this.panelSelectMervelleum = new PanelSelectSysteme();//, this.ctrl);
+		this.panelSelectQuintum = new PanelSelectSysteme();//, this.ctrl);
+
 		this.add(this.panelSelectTrialum);
+		this.add(this.panelSelectUninium);
+		this.add(this.panelSelectMervelleum);
+		this.add(this.panelSelectQuintum);
+
+		this.add(this.pnlCentre);
 	}
 
 }
