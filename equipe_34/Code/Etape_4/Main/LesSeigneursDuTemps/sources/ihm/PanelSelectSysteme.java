d682ee46559bc417c9103d5ba38af2a1fee16a59
Etienne Chevrollier <etienne.chevrollier@live.fr>
2021-06-10 23:53:30 +0200

    Systèmes qui se mettent à jour mais avec un bug d'affichage

--- equipe_34/Code/Etape_4/Main/LesSeigneursDuTemps/sources/ihm/PanelSelectSysteme.java
+++ equipe_34/Code/Etape_4/Main/LesSeigneursDuTemps/sources/ihm/PanelSelectSysteme.java
@@ -40,6 +40,15 @@ public class PanelSelectSysteme extends JPanel
 	private JButton btn5;
 	private JButton btn6;
 	private JButton btn7;
+	private JButton btn8;
+	private JButton btn9;
+	private JButton btn10;
+	private JButton btn11;
+	private JButton btn12;
+	private JButton btn13;
+	private JButton btn14;
+	private JButton btn15;
+	private JButton btn16;
 
 	private JPanel pnlPlanetes;
 
@@ -83,21 +92,35 @@ public class PanelSelectSysteme extends JPanel
 		//Systeme
 		this.imgSysteme = Toolkit.getDefaultToolkit().getImage( "./images/systemes/" + this.nomFichier + ".png" );
 
+		this.btn1 = PlaneteButton.planete(70, 75, "./images/planetes/bleu.png" );
+		this.btn2 = PlaneteButton.planete(200, 75, "./images/planetes/jaune.png" );
+		this.btn3 = PlaneteButton.planete(150, 175, "./images/planetes/vert.png" );
+		this.btn4 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
+		this.btn5 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
+		this.btn6 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
+		this.btn7 = PlaneteButton.planete(180, 170, "./images/planetes/bleu.png" );
+		this.btn8 = PlaneteButton.planete(125, 190, "./images/planetes/fuchsia.png" );
+		this.btn9 = PlaneteButton.planete(60, 165, "./images/planetes/vert.png" );
+		this.btn10 = PlaneteButton.planete(130, 120, "./images/planetes/orange.png" );
+		this.btn11 = PlaneteButton.planete(130, 115, "./images/planetes/rose.png" );
+		this.btn12 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
+		this.btn13 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
+		this.btn14 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
+		this.btn15 = PlaneteButton.planete(175, 170, "./images/planetes/bleu.png" );
+		this.btn16 = PlaneteButton.planete(70, 150, "./images/planetes/vert.png" );
+
 		//Planetes
 		switch(ctrl.getGalaxie().getSystemesSolaires().get(NumPlaceSysteme).getNom())
 		{
 			case "Triälum":
 					
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(70, 75, "./images/planetes/bleu.png" );
 				this.add(this.btn1);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(200, 75, "./images/planetes/jaune.png" );
 				this.add(this.btn2);
 
 				//Planete 3
-				this.btn3 = PlaneteButton.planete(150, 175, "./images/planetes/vert.png" );
 				this.add(this.btn3);
 			
 				break;
@@ -105,62 +128,49 @@ public class PanelSelectSysteme extends JPanel
 			case "Mervelléum":
 
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
-				this.add(this.btn1);
+				this.add(this.btn4);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
-				this.add(this.btn2);
-
-				//Planete 3
-				this.btn3 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
-				this.add(this.btn3);
-
-				//Planete 4
-				this.btn4 = PlaneteButton.planete(180, 170, "./images/planetes/bleu.png" );
-				this.add(this.btn4);
-
-				//Planete 5
-				this.btn5 = PlaneteButton.planete(125, 190, "./images/planetes/fuchsia.png" );
 				this.add(this.btn5);
 
-				//Planete 6
-				this.btn6 = PlaneteButton.planete(60, 165, "./images/planetes/vert.png" );
+				//Planete 3
 				this.add(this.btn6);
 
-				//Planete 7
-				this.btn7 = PlaneteButton.planete(130, 120, "./images/planetes/orange.png" );
+				//Planete 4
 				this.add(this.btn7);
+
+				//Planete 5
+				this.add(this.btn8);
+
+				//Planete 6
+				this.add(this.btn9);
+
+				//Planete 7
+				this.add(this.btn10);
 				
 				break;
 
 			case "Uninium":
 
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(130, 115, "./images/planetes/rose.png" );
-				this.add(this.btn1);
+				this.add(this.btn11);
 			
 				break;
 			case "Quintum":
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
-				this.add(this.btn1);
+				this.add(this.btn12);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
-				this.add(this.btn2);
+				this.add(this.btn13);
 
 				//Planete 3
-				this.btn3 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
-				this.add(this.btn3);
+				this.add(this.btn14);
 
 				//Planete 4
-				this.btn4 = PlaneteButton.planete(175, 170, "./images/planetes/bleu.png" );
-				this.add(this.btn4);
+				this.add(this.btn15);
 
 				//Planete 5
-				this.btn5 = PlaneteButton.planete(70, 150, "./images/planetes/vert.png" );
-				this.add(this.btn5);
+				this.add(this.btn16);
 				break;
 		}
 
@@ -189,80 +199,138 @@ public class PanelSelectSysteme extends JPanel
 		switch(ctrl.getGalaxie().getSystemesSolaires().get(NumPlaceSysteme).getNom())
 		{
 			case "Triälum":
-					
+
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(70, 75, "./images/planetes/bleu.png" );
 				this.add(this.btn1);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(200, 75, "./images/planetes/jaune.png" );
 				this.add(this.btn2);
 
 				//Planete 3
-				this.btn3 = PlaneteButton.planete(150, 175, "./images/planetes/vert.png" );
 				this.add(this.btn3);
+
+				this.btn4.setVisible(false);
+				this.btn5.setVisible(false);
+				this.btn6.setVisible(false);
+				this.btn7.setVisible(false);
+				this.btn8.setVisible(false);
+				this.btn9.setVisible(false);
+				this.btn10.setVisible(false);
+				this.btn11.setVisible(false);
+				this.btn12.setVisible(false);
+				this.btn13.setVisible(false);
+				this.btn14.setVisible(false);
+				this.btn15.setVisible(false);
+				this.btn16.setVisible(false);
+
+				this.btn1.setVisible(true);
+				this.btn2.setVisible(true);
+				this.btn3.setVisible(true);
 			
 				break;
 				
 			case "Mervelléum":
 
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
-				this.add(this.btn1);
+				this.add(this.btn4);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
-				this.add(this.btn2);
-
-				//Planete 3
-				this.btn3 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
-				this.add(this.btn3);
-
-				//Planete 4
-				this.btn4 = PlaneteButton.planete(180, 170, "./images/planetes/bleu.png" );
-				this.add(this.btn4);
-
-				//Planete 5
-				this.btn5 = PlaneteButton.planete(125, 190, "./images/planetes/fuchsia.png" );
 				this.add(this.btn5);
 
-				//Planete 6
-				this.btn6 = PlaneteButton.planete(60, 165, "./images/planetes/vert.png" );
+				//Planete 3
 				this.add(this.btn6);
 
-				//Planete 7
-				this.btn7 = PlaneteButton.planete(130, 120, "./images/planetes/orange.png" );
+				//Planete 4
 				this.add(this.btn7);
+
+				//Planete 5
+				this.add(this.btn8);
+
+				//Planete 6
+				this.add(this.btn9);
+
+				//Planete 7
+				this.add(this.btn10);
+
+				this.btn1.setVisible(false);
+				this.btn2.setVisible(false);
+				this.btn3.setVisible(false);
+				this.btn11.setVisible(false);
+				this.btn12.setVisible(false);
+				this.btn13.setVisible(false);
+				this.btn14.setVisible(false);
+				this.btn15.setVisible(false);
+				this.btn16.setVisible(false);
+
+				this.btn4.setVisible(true);
+				this.btn5.setVisible(true);
+				this.btn6.setVisible(true);
+				this.btn7.setVisible(true);
+				this.btn8.setVisible(true);
+				this.btn9.setVisible(true);
+				this.btn10.setVisible(true);
 				
 				break;
 
 			case "Uninium":
 
+				this.btn1.setVisible(false);
+				this.btn2.setVisible(false);
+				this.btn3.setVisible(false);
+				this.btn4.setVisible(false);
+				this.btn5.setVisible(false);
+				this.btn6.setVisible(false);
+				this.btn7.setVisible(false);
+				this.btn8.setVisible(false);
+				this.btn9.setVisible(false);
+				this.btn10.setVisible(false);
+				this.btn12.setVisible(false);
+				this.btn13.setVisible(false);
+				this.btn14.setVisible(false);
+				this.btn15.setVisible(false);
+				this.btn16.setVisible(false);
+
+				this.btn11.setVisible(true);
+
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(130, 115, "./images/planetes/rose.png" );
-				this.add(this.btn1);
+				this.add(this.btn11);
 			
 				break;
 			case "Quintum":
+
 				//Planete 1
-				this.btn1 = PlaneteButton.planete(60, 75, "./images/planetes/rouge.png" );
-				this.add(this.btn1);
+				this.add(this.btn12);
 				
 				//Planete 2
-				this.btn2 = PlaneteButton.planete(140, 40, "./images/planetes/jaune.png" );
-				this.add(this.btn2);
+				this.add(this.btn13);
 
 				//Planete 3
-				this.btn3 = PlaneteButton.planete(190, 100, "./images/planetes/rose.png" );
-				this.add(this.btn3);
+				this.add(this.btn14);
 
 				//Planete 4
-				this.btn4 = PlaneteButton.planete(175, 170, "./images/planetes/bleu.png" );
-				this.add(this.btn4);
+				this.add(this.btn15);
 
 				//Planete 5
-				this.btn5 = PlaneteButton.planete(70, 150, "./images/planetes/vert.png" );
-				this.add(this.btn5);
+				this.add(this.btn16);
+
+				this.btn1.setVisible(false);
+				this.btn2.setVisible(false);
+				this.btn3.setVisible(false);
+				this.btn4.setVisible(false);
+				this.btn5.setVisible(false);
+				this.btn6.setVisible(false);
+				this.btn7.setVisible(false);
+				this.btn8.setVisible(false);
+				this.btn9.setVisible(false);
+				this.btn10.setVisible(false);
+				this.btn11.setVisible(false);
+
+				this.btn12.setVisible(true);
+				this.btn13.setVisible(true);
+				this.btn14.setVisible(true);
+				this.btn15.setVisible(true);
+				this.btn16.setVisible(true);
+				
 				break;
 		}
 	}
